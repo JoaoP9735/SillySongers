87c3123729082b4647ec18032219101a4b30fcca
Gustag16 <gu.amadeu022@gmail.com>
2025-09-30 22:19:47 -0300

    fix

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -56,8 +56,16 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-# CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
 
 ROOT_URLCONF = 'myproject.urls'
 
